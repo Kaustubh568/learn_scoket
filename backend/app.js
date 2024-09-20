96c8855ce0925b2534c0a1bb3259e739c1c547a1
Kaustubh568 <chandelkaustubh781@gmail.com>
2024-09-20 20:16:39 +0530

    firstcommit

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,28 @@ const app = require('express')();
 const server = require('http').createServer(app);
 
 //now creating a connection with options
-const io = require('socket.io')(server);
+const io = require('socket.io')(server , {
+    cors: {
+      origin: "*",
+    //   allowedHeaders: ["my-custom-header"],
+    //   credentials: true
+     }
+});
 
 //we have just right now enabled the connection but this is npt enough you need to listen to certain events to pass on the certain information that you want
 io.on('connection',(socket) =>{
   console.log("what is socket", socket);
   console.log("socket has got the info");
 
-
   //events triggered by the client 
   socket.on('chat',(payload)=>{ 
     console.log('payload', payload);
-    //we also need to respon to this event 
+    //we also need to respond to this event 
     io.emit('chat',payload);
   });
 })
 
 
-server.listen(5000,()=>{
-   console.log("server is running on port 5000");
+server.listen(4000,()=>{
+   console.log("server is running on port 4000");
 });
--- frontend/chatty/src/App.js
+++ frontend/chatty/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+//to manage state we will use some of the useState we will be injecting a hok as well so we will also be using useEffect 
+
+import {useState,useEffect} from 'react';
+import io from 'socket.io-client';
+import {nanoid} from 'nanoid';
+
+const socket = io.connect('http://localhost:4000');
+const userName = nanoid(4);
+
 function App() {
+
+  const [messages,setMessages] = useState('');
+  const [chat, setChat] = useState([]); 
+  /*value is binded to messages state so whenever something is sent in the value it send to messages state . In onChange an event is given and as soon as an event is given a method will be fired up which will be called setMessages*/
+
+  const sendChat = (e)=>{
+    e.preventDefault();  //so that form doesnt get automatically send to different locations 
+    socket.emit('chat', {messages , userName})
+  }
+
+  useEffect(()=>{
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    })
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>chatty app</h1>
+      {chat.map((payload , index )=>{
+        return(
+          <p key={index}>{payload.messages}: <span>id: {payload.userName}</span></p>
+        )
+      })}
+      <form onSubmit={sendChat}>
+        <input type='text' name='chat' 
+          placeholder='send text'
+          value = {messages}
+          onChange={ (e) =>{setMessages(e.target.value)}}
+        />
+        <button type="submit">SEND</button>
+      </form>
     </div>
   );
+  /* so setmessage is responsible for changing the message and the actual value is the message , so anychange happening is using a method which is setMessage so seMessage is resposible to setting the value of actual message and the actual message is the value*/
 }
 
 export default App;
